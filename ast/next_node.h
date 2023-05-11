5ebbed43c2682d9119a9e88197e20a9f87f880b8
Diogo Melita <diogo.melita@tecnico.ulisboa.pt>
2023-05-11 21:25:14 +0100

    fix next and stop nodes, deleted program node and related code

--- ast/next_node.h
+++ ast/next_node.h
@@ -9,10 +9,15 @@ namespace mml {
    * Class for describing next nodes.
    */
   class next_node: public cdk::basic_node {
+    cdk::integer_node *_argument;
 
   public:
-    inline next_node(int lineno) :
-        cdk::basic_node(lineno){
+    inline next_node(int lineno, cdk::integer_node *argument) :
+        cdk::basic_node(lineno), _argument(argument) {
+    }
+
+    inline cdk::integer_node *argument() {
+      return _argument;
     }
 
     void accept(basic_ast_visitor *sp, int level) {
--- ast/stop_node.h
+++ ast/stop_node.h
@@ -9,10 +9,15 @@ namespace mml {
    * Class for describing stop nodes.
    */
   class stop_node: public cdk::basic_node {
+    cdk::integer_node *_argument;
 
   public:
-    inline stop_node(int lineno) :
-        cdk::basic_node(lineno){
+    inline stop_node(int lineno, cdk::integer_node *argument) :
+        cdk::basic_node(lineno), _argument(argument) {
+    }
+
+    inline cdk::integer_node *argument() {
+      return _argument;
     }
 
     void accept(basic_ast_visitor *sp, int level) {
