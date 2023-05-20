753e9dc21caf46b8cae4d01424a6ce64b72f51ad
Diogo Melita <diogo.melita@tecnico.ulisboa.pt>
2023-05-20 15:41:31 +0100

    small fix to xml and identity node

--- ast/identity_node.h
+++ ast/identity_node.h
@@ -11,8 +11,8 @@ namespace mml {
   class identity_node: public cdk::unary_operation_node {
 
   public:
-    inline identity_node(int lineno, cdk::expression_node* number) :
-            cdk::unary_operation_node(lineno, number) {
+    inline identity_node(int lineno, cdk::expression_node* argument) :
+            cdk::unary_operation_node(lineno, argument) {
     }
 
     void accept(basic_ast_visitor *sp, int level) {
